3ba762dd85a3b89bbbac4ea6ab5a1d4634a5a947
mr2cef <mr2cef@gmail.com>
2025-04-08 20:39:13 +0200

    adjust 2 my needs

--- easyBankcsv2qif.py
+++ easyBankcsv2qif.py
@@ -5,8 +5,8 @@ TODO: fill me
 
 based and inspired by http://code.google.com/p/xlscsv-to-qif/
 
-Author: berhard.denner@gmail.com
-Date: 16. Jan 2014
+Author: github@2cef.eu
+Date: 08.04.2025
 '''
 
 from __future__ import print_function
@@ -14,6 +14,7 @@ import sys
 import csv
 import re
 import argparse
+import datetime as dt
 
 # global var for enabling debugging
 doDebug = False
@@ -22,11 +23,11 @@ doDebug = False
 
 def createArgParser():
     parser = argparse.ArgumentParser(description='Convert an easybank or BAWAG CSV export file to QIF format')
+    parser.add_argument('type', choices=['CCard', 'Bank'],
+                        help='specify the csv type, default is Bank')
     parser.add_argument('file', help='input file in CSV format. If file is - sdtin is used')
     parser.add_argument('-o', '--output',
                         help="output file, to write the resulting QIF. If not given stdout is used")
-    parser.add_argument('-a', '--account',
-                        help="account name to use, if not given no account name information will be in the QIF")
     parser.add_argument('-d', '--debug', action="store_true",
                         help='print debugging information to stderr, this option includes -s')
     parser.add_argument('-s', '--summary', action="store_true",
@@ -37,6 +38,9 @@ def createArgParser():
                               http://docs.python.org/2/library/codecs.html#standard-encodings')
     parser.add_argument('-f', '--encfrom',
                         help='specify encoding for the input file')
+    parser.add_argument('--dateformat',
+                        help=r'specify the date format for the input file, default is "%%Y-%%m-%%d"')
+
 
     return parser
 
@@ -46,9 +50,10 @@ class Transaction(object):
     """ Transaction object, represents one transaction exratcted from the CSV
         file
     """
-    def __init__(self):
+    def __init__(self, csvtype: str):
         object.__init__(self)
         self.account = ""
+        self.csvtype = csvtype
         self.description = ""
         self.date = ""
         self.valutadate = ""
@@ -62,14 +67,15 @@ class Transaction(object):
         self.htype = ""
         self.desc1 = ""
         self.desc2 = ""
+        self.types = set()
 
 
     def setTransaction(self, account, description, date, valutadate, amount, currency):
         self.account = account
         self.description = description
-        self.date = date
+        self.date = dt.datetime.strptime(date, '%d.%m.%Y').strftime('%Y-%m-%d')
         self.valutadate = valutadate
-        self.amount = amount
+        self.amount = amount.replace('.', '').replace(',', '.')
         self.currency = currency
         self.parseDescription()
 
@@ -77,128 +83,148 @@ class Transaction(object):
     def parseDescription(self):
         """ parses the description field to get more detailed information
         """
-        r = re.match("^(.*)\W*([A-Z]{2})/([0-9]+)\W*(.*)?$", self.description)
-        if r is not None:
-            self.desc1 = r.group(1).strip()
-            self.type = r.group(2)
-            self.id = r.group(3)
-            self.desc2 = r.group(4).strip()
+        if self.csvtype == "CCard":
+            d_list = self.description.split("|")
+            self.payee = d_list[0].strip()
+            if len(d_list) > 1:
+                self.desc1 = d_list[1].strip()
+            self.memo = self.description
+        if self.csvtype == "Bank":
+            r = re.match(r"^(.*)\W*([A-Z]{2})/([0-9]+)\W*(.*)?$", self.description)
+            if r is not None:
+                self.desc1 = r.group(1).strip()
+                self.type = r.group(2)
+                self.id = r.group(3)
+                self.desc2 = r.group(4).strip().replace('|', ' ')
+                print('tpye: {}\ndesc1: {}\ndesc2: {}\nid: {}'.format(self.type, self.desc1, self.desc2, self.id), file=sys.stderr)
 
-            # Cash withdraw
-            if (self.type == "BG" and \
-                (self.desc1 == "Auszahlung Karte" or self.desc1 == "Auszahlung Maestro")):
-                self.htype = "withdraw"
-                self.memo = "{} {}".format(self.desc1, self.desc2)
-
-            # transfer
-            elif (self.type == "BG" or self.type == "FE") \
-               and len(self.desc2) > 0 and len(self.desc1) > 0:
-                self.htype = "transfer"
-                m = re.match("^(([A-Z0-9]+\W)?[A-Z]*[0-9]+)\W(\w+\W+\w+)\W*(.*)$", self.desc2)
-                if m is not None:
-                    self.payee = m.group(3) + " (" + m.group(1) + ")"
-                    self.desc2 = m.group(1)
-                    self.memo = self.desc1 + " " + m.group(4)
-                else:
-                    m = re.match("^([A-Z]{2}[0-9]{18})\s+(.*)$", self.desc2)
-                    if m is not None:
-                        self.payee = "{} ({})".format(m.group(2), m.group(1))
-                        self.desc2 = m.group(2)
-                        self.memo = m.group(2)
-
-            # BG can meen "Bankgebuehren" (bankfee), therefore the desc2 XOR desc1 is empty
-            elif (self.type in ["BG", "RI"] and len(self.desc2) == 0):
-                self.memo = self.desc1
-                self.htype = "bankfee"
-
-            elif (self.type in ["BG", "RI"] and len(self.desc1) == 0):
-                self.memo = self.desc2
-                self.htype = "bankfee"
-
-            # not really a transfer, but use the information we have
-            elif self.type == "MC" and len(self.desc1) == 0:
-                self.memo = self.desc2
-
-            elif self.type == "MC" and len(self.desc2) == 0:
-                self.memo = self.desc1
-
-            # Maestro card (cash card) things
-            elif self.type == "MC" \
-                and len(self.desc1) > 0 and len(self.desc2) > 0:
-                # withdraw with cash card
-                m1 = re.match("^((Auszahlung)\W+\w+)\W*(.*)$", self.desc1)
-
-                # payment with cash card
-                m2 = re.match("^((Bezahlung)\W+\w+)\W*(.*)$", self.desc1)
-
-                if m1 is not None:
+                # Cash withdraw
+                if (self.type == "BG" and \
+                    (self.desc1 == "Auszahlung Karte" or self.desc1 == "Auszahlung Maestro")):
                     self.htype = "withdraw"
-                    self.memo = m1.group(1)
-                    if len(m1.group(3)) > 0:
-                        self.memo += " (" + m1.group(3) + ")"
-                    self.memo += " " + self.desc2
-
-                elif m2 is not None:
-                    self.htype = "payment"
-                    self.memo = m2.group(1)
-                    if len(m2.group(3)) > 0:
-                        self.memo += " (" + m2.group(3) + ")"
-                    self.memo += " " + self.desc2
-
-                else:
-                    # credit card bill and other infos
-                    if re.match("^Abrechnung.*", self.desc2):
-                        self.htype = "credit card bill"
-                    else:
-                        self.htype = "unknown"
-
                     self.memo = "{} {}".format(self.desc1, self.desc2)
 
-
-            # mixture of transfer, cash card payments
-            elif self.type == "VD":
-                # if we have a value for desc1 but not for desc2
-                # it may be a cash card payment
-                if len(self.desc1) > 0 and len(self.desc2) == 0:
-                    self.htype = "payment"
-                    self.memo = self.desc1
-
-                # if we have values for both desc fields it may be a transfer
-                elif len(self.desc1) > 0 and len(self.desc2) > 0:
+                # transfer
+                elif (self.type == "BG" or self.type == "FE") \
+                   and len(self.desc2) > 0 and len(self.desc1) > 0:
                     self.htype = "transfer"
-                    self.memo = self.desc1
-                    m = re.match("^(([A-Z0-9]+\W)?[A-Z]*[0-9]+)?\W*(\w+\W+\w+)\W*(.*)$", self.desc2)
+                    m = re.match(r"^(([A-Z0-9]+\W)?[A-Z]*[0-9]+)\W(\w+\W+\w+)\W*(.*)$", self.desc2)
                     if m is not None:
-                        self.payee = m.group(3)
-                        if m.group(1) is not None:
-                            self.payee += " (" + m.group(1) + ")"
+                        self.payee = m.group(3) + " (" + m.group(1) + ")"
+                        self.desc2 = m.group(1)
+                        self.memo = self.desc1 + " " + m.group(4)
+                    else:
+                        m = re.match(r"^([A-Z]{2}[0-9]{18})\s+(.*)$", self.desc2)
+                        if m is not None:
+                            self.payee = "{} ({})".format(m.group(2), m.group(1))
+                            self.desc2 = m.group(2)
+                            self.memo = m.group(2)
 
-                        self.memo += " " + m.group(4)
+                # BG can meen "Bankgebuehren" (bankfee), therefore the desc2 XOR desc1 is empty
+                elif (self.type in ["BG", "RI"] and len(self.desc2) == 0):
+                    self.memo = self.desc1
+                    self.htype = "bankfee"
 
-            # OG also seems to be a payment transaction
-            elif self.type == "OG":
-                self.htype = "payment"
-                # here we have desc1 and desc2
-                self.memo = "{} {}".format(self.desc1, self.desc2)
+                elif (self.type in ["BG", "RI"] and len(self.desc1) == 0):
+                    self.memo = self.desc2
+                    self.htype = "bankfee"
+
+                # not really a transfer, but use the information we have
+                elif self.type == "MC" and len(self.desc1) == 0:
+                    self.memo = self.desc2
+
+                elif self.type == "MC" and len(self.desc2) == 0:
+                    self.memo = self.desc1
+
+                # Maestro card (cash card) things
+                elif self.type == "MC" \
+                    and len(self.desc1) > 0 and len(self.desc2) > 0:
+                    # withdraw with cash card
+                    m1 = re.match(r"^((Auszahlung)\W+\w+)\W*(.*)$", self.desc1)
+
+                    # payment with cash card
+                    m2 = re.match(r"^((Bezahlung)\W+\w+)\W*(.*)$", self.desc1)
+
+                    if m1 is not None:
+                        self.htype = "withdraw"
+                        self.memo = m1.group(1)
+                        if len(m1.group(3)) > 0:
+                            self.memo += " (" + m1.group(3) + ")"
+                        self.memo += " " + self.desc2
+
+                    elif m2 is not None:
+                        self.htype = "payment"
+                        self.memo = m2.group(1)
+                        if len(m2.group(3)) > 0:
+                            self.memo += " (" + m2.group(3) + ")"
+                        self.memo += " " + self.desc2
+
+                    else:
+                        # credit card bill and other infos
+                        if re.match(r"^Abrechnung.*", self.desc2):
+                            self.htype = "credit card bill"
+                        else:
+                            self.htype = "unknown"
+
+                        self.memo = "{} {}".format(self.desc1, self.desc2)
 
 
-            # seems to be an cash card payment, however, I don't have enough
-            # infos about it
-            #elif self.type == "OG":
+                # mixture of transfer, cash card payments
+                elif self.type == "VD":
+                    # if we have a value for desc1 but not for desc2
+                    # it may be a cash card payment
+                    if len(self.desc1) > 0 and len(self.desc2) == 0:
+                        self.htype = "payment"
+                        self.memo = self.desc1
+
+                    # if we have values for both desc fields it may be a transfer
+                    elif len(self.desc1) > 0 and len(self.desc2) > 0:
+                        self.htype = "transfer"
+                        self.memo = self.desc1
+                        m = re.match(r"^(([A-Z0-9]+\W)?[A-Z]*[0-9]+)?\W*(\w+\W+\w+)\W*(.*)$", self.desc2)
+                        if m is not None:
+                            self.payee = m.group(3)
+                            #if m.group(1) is not None:
+                            #    self.payee += " (" + m.group(1) + ")"
+
+                            self.memo += " " + m.group(4)
+
+                # OG also seems to be a payment transaction
+                elif self.type == "OG":
+                    self.htype = "payment"
+                    if len(self.desc1) > 0 and len(self.desc2) > 0:
+                        self.memo = self.desc1
+                        m = re.match(r"^(([A-Z0-9]+\W)?[A-Z]*[0-9]+)?\W*(\w+\W+\w+)\W*(.*)$", self.desc2)
+                        if m is not None:
+                            self.payee = m.group(3) + ' ' + m.group(4)
+                            #if m.group(1) is not None:
+                            #    self.payee += " (" + m.group(1) + ")"
+                            self.memo += " " + m.group(4)
+                    else:
+                        # here we have desc1 and desc2
+                        self.memo = "{} {}".format(self.desc1, self.desc2)
+
+
+                # seems to be an cash card payment, however, I don't have enough
+                # infos about it
+                #elif self.type == "OG":
+                else:
+                    # use what we have
+                    self.memo = self.desc1 + " " + self.desc2
+
+            # if we got an unkown description field, use it as memo
             else:
-                # use what we have
-                self.memo = self.desc1 + " " + self.desc2
+                self.memo = self.description
 
-        # if we got an unkown description field, use it as memo
-        else:
-            self.memo = self.description
+            if self.htype == "":
+                self.htype = 'unknown'
 
-        if self.htype == "":
-            self.htype = 'unknown'
-
-        # finally, some clean up
-        self.memo = self.cleanStr(self.memo)
-        self.payee = self.cleanStr(self.payee)
+            # finally, some clean up
+            self.memo = self.cleanStr(self.description)
+            self.payee = self.cleanStr(self.payee)
+            print(self.payee)
+            print()
+            print()
 
 
     def cleanStr(self, string):
@@ -253,20 +279,15 @@ class EasyCSV2QIFconverter:
     """ create for each row of the given CSV a Transaction object
         a outputs this Transaction object to the given output file stream
     """
-    def __init__(self, instream, outstream, account=''):
+    def __init__(self, instream, outstream, csvtype, dateformat='%Y-%m-%d'):
         self._instream = instream
         self._outstream = outstream
-        self._account = account
+        self._csvtype = csvtype
         self._transSummary = {}
+        self._dateformat = dateformat
 
     def convert(self):
-        if self._account is not None:
-            print('!Account',
-                  'N{}'.format(self._account),
-                  'Tcash',
-                  '^',
-                  '!Type:Bank',
-                  sep='\n', file=self._outstream)
+        print(f'!Type:{self._csvtype}', file=self._outstream)
 
         delimiter = ';'
         rows = csv.reader(self._instream, delimiter=delimiter)
@@ -275,7 +296,7 @@ class EasyCSV2QIFconverter:
                 print ('ignoring invalid line:', l, file=sys.stderr)
                 continue
 
-            t = Transaction()
+            t = Transaction(self._csvtype)
             t.setTransaction(l[0],  # account
                              l[1],  # description
                              l[2],  # date
@@ -340,7 +361,7 @@ if __name__ == "__main__":
     else:
         outstream = sys.stdout
 
-    converter = EasyCSV2QIFconverter(instream, outstream, args.account)
+    converter = EasyCSV2QIFconverter(instream, outstream, args.type, args.dateformat)
 
     converter.convert()
     if args.debug or args.summary:
